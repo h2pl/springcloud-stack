a97532a9b382d4acedb56a0b9b1c580ff6ab06c4
h2pl <362294931@qq.com>
2022-09-11 16:39:26 +0800

    init commit

---
+++ src/main/java/com/hpl/springcloudstack/SpringcloudStackApplication.java
@@ -0,0 +1,13 @@
+package com.hpl.springcloudstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hpl/springcloudstack/SpringcloudStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpl.springcloudstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
