85be0d33496aa656da29258822e8475820845ff3
h2pl <362294931@qq.com>
2022-09-12 09:19:23 +0800

    replace nacos of eureka

--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication(scanBasePackages = {"com.example.*"})
 @MapperScan("com.example.orderservice.**.mapper")
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableFeignClients
 public class OrderServiceApplication {
 
--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,14 +3,14 @@ package com.example.userservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hpl
  */
 @SpringBootApplication(scanBasePackages = {"com.example.*"})
 @MapperScan("com.example.userservice.**.mapper")
-@EnableEurekaClient
+//@EnableEurekaClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
