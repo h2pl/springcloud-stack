3a6804220e782724e6d7d911a881775e00873b7f
h2pl <362294931@qq.com>
2022-09-12 09:54:50 +0800

    add ribbon loadbalance

--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.orderservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @MapperScan("com.example.orderservice.**.mapper")
 //@EnableEurekaClient
 @EnableFeignClients
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     @Bean
