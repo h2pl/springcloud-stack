42aa417da0b8e504335703e110fd198b2a763853
h2pl <362294931@qq.com>
2022-09-12 15:00:55 +0800

    add global filter and cors config

---
+++ gateway/src/main/java/com/example/gateway/AuthorizeFilter.java
@@ -0,0 +1,36 @@
+package com.example.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author hpl
+ * @date 2022/9/12 14:32
+ */
+@Component
+public class AuthorizeFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        MultiValueMap<String,String> params = request.getQueryParams();
+        String auth = params.getFirst("authorization");
+        if ("admin".equals(auth)) {
+            return chain.filter(exchange);
+        }
+
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
